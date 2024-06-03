8ab2a9bfced535b7e90e37d9fdcd0ab5663239bb
DESKTOP-5FBE16T\User <gwon.park@samsung.com>
2024-06-03 16:09:52 +0900

    Add NemoDriver Class

---
+++ Project2/NemoDriver.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include "NemoAPI.cpp"
+#include "StockBrockerDriverInterface.h"
+
+using namespace std;
+
+class NemoDriver : public StockBrockerDriverInterface {
+public:
+	NemoDriver(NemoAPI nemoApi) :
+		_nemoApi(nemoApi) {}
+
+	bool login(string str1, string str2) {}
+	bool buy(string stockCode, int amount, int price) {
+		_nemoApi.purchasingStock(stockCode, amount, price);
+
+		return true;
+	}
+	bool sell(string stockCode, int amount, int price) {}
+	int getCurrentPrice(string stockCode) {}
+
+private:
+	NemoAPI _nemoApi;
+};
