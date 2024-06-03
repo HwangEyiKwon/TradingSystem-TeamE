e805d0939146808a84510d292909ddb3eae961e5
DESKTOP-5FBE16T\User <user@user.com>
2024-06-03 15:50:17 +0900

    [feature] auto trading system skeleton

---
+++ Project2/KiwerDriver.cpp
@@ -0,0 +1,17 @@
+#include "KiwerDriver.h"
+
+bool KiwerDriver::login(string str1, string str2) {
+	return true;
+}
+
+bool KiwerDriver::buy(string stockCode, int amount, int price) {
+	return true;
+}
+
+bool KiwerDriver::sell(string stockCode, int amount, int price) {
+	return true;
+}
+
+int KiwerDriver::getCurrentPrice(string stockCode) {
+	return 0;
+}
---
+++ Project2/NemoDriver.cpp
@@ -0,0 +1,17 @@
+#include "NemoDriver.h"
+
+bool NemoDriver::login(string str1, string str2) {
+	return true;
+}
+
+bool NemoDriver::buy(string stockCode, int amount, int price) {
+	return true;
+}
+
+bool NemoDriver::sell(string stockCode, int amount, int price) {
+	return true;
+}
+
+int NemoDriver::getCurrentPrice(string stockCode) {
+	return 0;
+}
