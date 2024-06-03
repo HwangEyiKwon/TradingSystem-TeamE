7b66d5ebc19cea5e95cefdc27424bee82dd0673e
DESKTOP-5FBE16T\User <user@user.com>
2024-06-03 16:00:49 +0900

    [feature] login green

--- Project2/AutoTradingSystem.cpp
+++ Project2/AutoTradingSystem.cpp
@@ -1,8 +1,11 @@
 #include "AutoTradingSystem.h"
+#include <stdexcept>
 
 AutoTradingSystem::AutoTradingSystem(StockBrockerDriverInterface* _driver)
 	: driver(_driver) {}
 
 bool AutoTradingSystem::login(string id, string pwd) {
+	if (id == "") throw std::invalid_argument("id should be entered");
+	if (pwd == "") throw std::invalid_argument("pwd should be entered");
 	return true;
 }
