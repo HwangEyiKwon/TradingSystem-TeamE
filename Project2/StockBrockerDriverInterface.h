0e738eb65c25f31e92c74aa2d0186608fdb866f4
DESKTOP-5FBE16T\User <gwon.park@samsung.com>
2024-06-03 15:56:06 +0900

    Add StockBrockerDriverInterface

--- Project2/StockBrockerDriverInterface.h
+++ Project2/StockBrockerDriverInterface.h
@@ -1,1 +1,11 @@
 #pragma once
+#include <string>
+
+using namespace std;
+
+class StockBrockerDriverInterface {
+	virtual bool login(string str1, string str2) = 0;
+	virtual bool buy(string stockCode, int amount, int price) = 0;
+	virtual bool sell(string stockCode, int amount, int price) = 0;
+	virtual int getCurrentPrice(string stockCode) = 0;
+};
